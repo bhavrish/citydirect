eaf5e6d46ad2f3bad1905e99988efb9e6926d4d1
Anthony Bravo <bravo@Anthonys-MBP.fios-router.home>
2018-11-17 19:39:54 -0500

    Added Home Page

---
+++ NacDirect/TableViewCells/FloorCell.swift
@@ -0,0 +1,26 @@
+//
+//  FloorCell.swift
+//  NacDirect
+//
+//  Created by Anthony Bravo on 11/17/18.
+//  Copyright © 2018 Bhavesh Shah. All rights reserved.
+//
+
+import UIKit
+
+class FloorCell: UITableViewCell {
+
+    @IBOutlet weak var floorNumber: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NacDirect/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SignUpViewController.swift
+//  NacDirect
+//
+//  Created by Ishraq Khan on 11/17/18.
+//  Copyright © 2018 Bhavesh Shah. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var registerButtonFrame: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // set register button properties
+        registerButtonFrame.layer.cornerRadius = 8.0
+        registerButtonFrame.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onRegister(_ sender: Any) {
+        // initializes a new Parse user object
+        let newUser = PFUser()
+        
+        // sets user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.email = emailField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User created")
+                self.performSegue(withIdentifier: "homeSegue2", sender: nil)
+            }
+            
+        }
+    }
+
+}
